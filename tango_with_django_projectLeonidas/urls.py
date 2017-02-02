0fb1b7c52208f92d78973bd334b3cc7b970cb2e4
Leonidas Reppas <2202389r@student.gla.ac.uk>
2017-02-02 18:22:39 +0000

    All the files until the start of chpter 5

--- tango_with_django_projectLeonidas/urls.py
+++ tango_with_django_projectLeonidas/urls.py
@@ -1,5 +1,4 @@
 """tango_with_django_projectLeonidas URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
 Examples:
