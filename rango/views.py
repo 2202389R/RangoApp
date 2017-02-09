afe542eeb1656be654e87757f8a6b878c4b9092c
Leonidas Reppas <2202389r@student.gla.ac.uk>
2017-02-09 22:22:32 +0000

    Chapter 10 done everything before 10.5

--- rango/views.py
+++ rango/views.py
@@ -12,6 +12,7 @@ from django.contrib.auth import logout
 
 
 def index(request):
+    request.session.set_test_cookie()
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list, 'pages': page_list}
@@ -20,6 +21,9 @@ def index(request):
 
 
 def about(request):
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+    request.session.delete_test_cookie()
     print(request.method)
     print(request.user)
     return render(request, 'rango/about.html', {})
